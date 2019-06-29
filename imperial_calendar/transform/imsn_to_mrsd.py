39a494f12c039151073e2fc0a927f68e05dd4bf1
Tachibana Haruna <tachibana.haruna@gmail.com>
2019-06-29 21:17:40 +0900

    閏年の法則を變更した
    以前は火星の1年=668.594火星日を採用し、此れに基づいて置閏法を設定してゐたが、近年の資料に基づき冬至囘歸年=668.596火星日を1年の長さとして採用し、其れに合せて置閏法、及び帝國火星日の元期を變更した。

--- imperial_calendar/transform/imsn_to_mrsd.py
+++ imperial_calendar/transform/imsn_to_mrsd.py
@@ -5,4 +5,5 @@ from imperial_calendar.MarsSolDate import MarsSolDate
 
 def imsn_to_mrsd(imsn: ImperialSolNumber) -> MarsSolDate:
     """帝國火星日からMSDを算出する."""
-    return MarsSolDate(imsn.imperial_sol_number + 0.375 - 901193)
+    return MarsSolDate(imsn.imperial_sol_number + 0.375 - 901195)
+    #return MarsSolDate(imsn.imperial_sol_number + 0.375 - 901193)
