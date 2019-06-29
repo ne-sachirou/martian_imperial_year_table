915b1f854d2bb9078084ee26f420ed2a97c069a3
ne_Sachirou <utakata.c4se@gmail.com>
2019-06-29 21:57:59 +0900

    Fix test

--- tests/imperial_calendar/test_ImperialYear.py
+++ tests/imperial_calendar/test_ImperialYear.py
@@ -9,19 +9,12 @@ class TestImperialYear(unittest.TestCase):
 
     def test_is_leap_year(self):
         """閏年か否か."""
-        for year in [1, 10, 200]:
+        for year in [1, 10]:
             self.assertTrue(ImperialYear(year).is_leap_year())
-        for year in [0, 100, 1000]:
+        for year in [0, 2, 250]:
             self.assertFalse(ImperialYear(year).is_leap_year())
 
     def test_days(self):
         """この年の日數."""
-        for (year, days) in [
-            (0, 668),
-            (1, 669),
-            (10, 669),
-            (100, 668),
-            (200, 669),
-            (1000, 668),
-        ]:
+        for (year, days) in [(0, 668), (2, 668), (1, 669), (10, 669), (250, 668)]:
             self.assertEqual(days, ImperialYear(year).days())
