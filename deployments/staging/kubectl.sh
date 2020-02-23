8fa56f0a65799a4cf376547e981a80b378cd0bd9
ne_Sachirou <utakata.c4se@gmail.com>
2020-02-24 00:36:24 +0900

    New K8s cluster

--- deployments/staging/kubectl.sh
+++ deployments/staging/kubectl.sh
@@ -2,15 +2,7 @@
 
 set -eux
 
-# https://github.com/kubernetes-sigs/kustomize/blob/master/docs/INSTALL.md
-curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |\
-  grep browser_download |\
-  grep linux |\
-  cut -d '"' -f 4 |\
-  grep /kustomize/v |\
-  sort | tail -n 1 |\
-  xargs -t curl -O -L
-tar vxzf ./kustomize_v*_linux_amd64.tar.gz
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash -eux
 mv kustomize /usr/local/bin
 
 cd deployments/staging || exit 1
@@ -19,7 +11,7 @@ echo -n "$MACKEREL_APIKEY" > secret/MACKEREL_APIKEY
 kustomize edit set image "gcr.io/${PROJECT_ID}/martian_imperial_year_table=gcr.io/${PROJECT_ID}/martian_imperial_year_table:${SHORT_SHA}"
 kustomize build | \
 /builder/kubectl.bash apply \
-  -n staging \
+  -n martian-imperial-year-table-staging \
   -l app=martian-imperial-year-table \
   --prune \
   -f -
