3e32c2f80682ae477b0090188ad20d7700e38163
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-10-02 04:51:54 +0900

    最近Cloud Buildの開始が遲いので開始を待つ秒數をs/20秒/30秒/に延ばす

--- tasks.py
+++ tasks.py
@@ -183,7 +183,7 @@ def deploy_staging():
     run("git tag -f staging")
     run("git push -f origin staging")
     with powershell() as _run:
-        time.sleep(20)
+        time.sleep(30)
         builds_filter = """
         source.repoSource.repoName = github_ne-sachirou_martian_imperial_year_table AND
         source.repoSource.tagName = staging
@@ -216,7 +216,7 @@ def deploy_production():
     run("git tag -f production")
     run("git push -f origin production")
     with powershell() as _run:
-        time.sleep(20)
+        time.sleep(30)
         builds_filter = """
         source.repoSource.repoName = github_ne-sachirou_martian_imperial_year_table AND
         source.repoSource.tagName = production
